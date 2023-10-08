6837b01bb5dfc1ab26c4ea2a766e485965c1d389
sungeunnn <fronteun@naver.com>
2023-10-08 15:47:57 +0900

    [feat] Coin image rendering using API

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,42 +1,42 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components"
+import styled from "styled-components";
 
 const Container = styled.div`
-  padding:0px 20px;
+  padding: 0px 20px;
   max-width: 480px;
   margin: 0 auto;
 `;
 
 const Header = styled.header`
-  height :10vh;
+  height: 10vh;
   display: flex;
   justify-content: center;
-  align-items: center;  
+  align-items: center;
 `;
 
 const CoinsList = styled.ul``;
 
 const Coin = styled.li`
-  background-color : white;
-  color: ${props => props.theme.bgColor};
-  border-radius : 15px;
-  margin-bottom : 10px;
-  a{
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  border-radius: 15px;
+  margin-bottom: 10px;
+  a {
     padding: 20px;
     transition: color 0.2s ease-in;
-    display:block;
+    display: block;
   }
   &:hover {
-    a{
-      color:${props => props.theme.accentColor}
+    a {
+      color: ${(props) => props.theme.accentColor};
     }
   }
 `;
 
 const Title = styled.h1`
-  color : ${props => props.theme.titleColor};
-  font-size : xx-large;
+  color: ${(props) => props.theme.titleColor};
+  font-size: xx-large;
 `;
 
 const Loader = styled.span`
@@ -44,15 +44,21 @@ const Loader = styled.span`
   display: block;
 `;
 
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+`;
+
 //타입스크립트 Coin의 각 타입 명시
-interface CoinInterface{
-    id: string,
-    name: string,
-    symbol: string,
-    rank: number,
-    is_new: boolean,
-    is_active: boolean,
-    type: string
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
 }
 
 function Coins() {
@@ -78,12 +84,16 @@ function Coins() {
           <CoinsList>
             {coins.map((coin) => (
               <Coin key={coin.id}>
-                <Link to={`coin.id`}>{coin.name} &rarr;</Link>
-              </Coin>))}
+                <Link to={`/${coin.id}`} state={{ name: coin.name }}>
+                  <Img src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`}></Img>
+                  {coin.name} &rarr;
+                </Link>
+              </Coin>
+            ))}
           </CoinsList>
         )}
       </Container>
     </>
-  )
+  );
 }
 export default Coins;
